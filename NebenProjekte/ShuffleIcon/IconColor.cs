0b08ccbf6cc3de32583d05152d8295a1f5dded96
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2019-05-13 08:07:25 +0200

    Improved ShuffleIcon generator

---
+++ NebenProjekte/ShuffleIcon/IconColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+
+namespace ShuffleIcon
+{
+    struct IconColor
+    {
+        public string Name { get; }
+
+        public Color Color { get; }
+
+        public Brush Brush { get; }
+
+        public IconColor(string name, Color color)
+        {
+            Name = name;
+            Color = color;
+            Brush = new SolidBrush(color);
+        }
+    }
+}
--- NebenProjekte/ShuffleIcon/Program.cs
+++ NebenProjekte/ShuffleIcon/Program.cs
@@ -1,51 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ShuffleIcon
 {
     class Program
     {
-        private const int f = 35, t = 25, a = 250, c = 200, h = 180;
-        private const double s = 1.009;
+        private const int f = 35, t = 25, a = 250, c = 190, h = 180;
+        private const double s = 1.0393;
         private static readonly Color deleteColor = Color.Red;
 
-        // Colors: Blue, Orange, Black, White, GrayForLight, GrayForDark
-        private static readonly string colorName = "Blue";
-        private static readonly Color drawColor = Color.Blue;
-        private static readonly Brush drawBrush = Brushes.Blue;
-
-        //private static readonly string colorName = "Orange";
-        //private static readonly Color drawColor = Color.Orange;
-        //private static readonly Brush drawBrush = Brushes.Orange;
-
-        //private static readonly string colorName = "Black";
-        //private static readonly Color drawColor = Color.Black;
-        //private static readonly Brush drawBrush = Brushes.Black;
-
-        //private static readonly string colorName = "White";
-        //private static readonly Color drawColor = Color.White;
-        //private static readonly Brush drawBrush = Brushes.White;
-
-        //private static readonly string colorName = "DarkGray";
-        //private static readonly Color drawColor = Color.Gray;
-        //private static readonly Brush drawBrush = Brushes.Gray;
-
-        //private static readonly string colorName = "LightGray";
-        //private static readonly Color drawColor = Color.LightGray;
-        //private static readonly Brush drawBrush = Brushes.LightGray;
+        private static readonly IconColor[] colors = new IconColor[]
+        {
+            new IconColor( "Black", Color.Black),
+            new IconColor( "White", Color.White),
+            new IconColor( "Blue", Color.Blue),
+            new IconColor( "Yellow", Color.Yellow),
+            new IconColor( "Gray", Color.Gray),
+            new IconColor( "LightGray", Color.LightGray)
+        };
 
 
         static void Main(string[] args)
         {
+            foreach (IconColor color in colors)
+            {
+                GenerateIcon(color);
+            }
+        }
+
+        private static void GenerateIcon(IconColor color)
+        {
+            Console.WriteLine(color.Name);
+
             Bitmap bmp = new Bitmap(a * f, a * f);
 
             using (Graphics g = Graphics.FromImage(bmp))
             {
-                Bitmap bmpSquare = GetSquareIcon(a, c);
+                Bitmap bmpSquare = GetSquareIcon(a, c, color);
                 Bitmap bmpNotSquare = new Bitmap(bmpSquare, a * f, h * f);
 
                 g.DrawImage(bmpNotSquare, 0 * f, (a - h) / 2 * f);
@@ -55,18 +46,19 @@ namespace ShuffleIcon
             }
 
             bmp = new Bitmap(bmp, new Size(a, a));
-            bmp.Save(string.Format("ShuffleIcon{0}.png", colorName), System.Drawing.Imaging.ImageFormat.Png);
+            bmp.Save(string.Format("ShuffleIcon{0}.png", color.Name), System.Drawing.Imaging.ImageFormat.Png);
         }
 
-        private static Bitmap GetSquareIcon(int a, int crossSize)
+        private static Bitmap GetSquareIcon(int a, int crossSize, IconColor color)
         {
             int arrowMiddleY;
             Bitmap bmp = new Bitmap(a * f, a * f);
 
             using (Graphics g = Graphics.FromImage(bmp))
             {
-                Bitmap bmpCross = GetCross(crossSize, out arrowMiddleY);
-                g.DrawImage(bmpCross, new Point(0 * f, 50 * f));
+                int yCrossOffset = (a - crossSize - t) / 2;
+                Bitmap bmpCross = GetCross(crossSize, color, out arrowMiddleY);
+                g.DrawImage(bmpCross, 0 * f, yCrossOffset * f);
 
                 bmpCross.Dispose();
 
@@ -74,59 +66,67 @@ namespace ShuffleIcon
                 Point[] arrowDownPoints = new Point[3];
 
                 arrowUpPoints[0] = new Point(crossSize * f, 0 * f);
-                arrowUpPoints[1] = new Point(crossSize * f, arrowMiddleY * 2 * f);
-                arrowUpPoints[2] = new Point(a * f, arrowMiddleY * f);
+                arrowUpPoints[1] = new Point(crossSize * f, (yCrossOffset + arrowMiddleY) * 2 * f);
+                arrowUpPoints[2] = new Point(a * f, (yCrossOffset + arrowMiddleY) * f);
 
                 arrowDownPoints[0] = new Point(crossSize * f, a * f);
-                arrowDownPoints[1] = new Point(crossSize * f, (a - arrowMiddleY * 2) * f);
-                arrowDownPoints[2] = new Point(a * f, (a - arrowMiddleY) * f);
+                arrowDownPoints[1] = new Point(crossSize * f, (a - (arrowMiddleY + yCrossOffset) * 2) * f);
+                arrowDownPoints[2] = new Point(a * f, (a - arrowMiddleY - yCrossOffset) * f);
 
-                g.FillPolygon(drawBrush, arrowDownPoints);
-                g.FillPolygon(drawBrush, arrowUpPoints);
+                g.FillPolygon(color.Brush, arrowDownPoints);
+                g.FillPolygon(color.Brush, arrowUpPoints);
             }
 
             return bmp;
         }
 
-        private static Bitmap GetCross(int a, out int yEnd)
+        private static Bitmap GetCross(int a, IconColor color, out int yEnd)
         {
             int w = a, h = a + t;
             Bitmap bmp = new Bitmap(w * f, h * f);
 
+            yEnd = 0;
+
             using (Graphics g = Graphics.FromImage(bmp))
             {
-                int middleX, iWhenXIsW = 0;
-
-                middleX = MiddleX(w, s);
-
+                double middleX = MiddleX(w, s);
                 Point[] pointsDown = new Point[w + t];
                 Point[] pointsUp = new Point[w + t];
+                double calcX = middleX - pointsDown.Length / 2.0;
+                int calcCount = (int)Math.Ceiling(pointsDown.Length / 2.0);
 
-                for (int i = 0, x = middleX - w / 2 - 50; i < pointsDown.Length; i++)
+                for (int i = 0; i < calcCount; i++, calcX++)
                 {
-                    int yDown = (Y(w - t, s, x++) + t);
-                    pointsDown[i] = new Point(i * f, yDown * f);
-                    pointsUp[i] = new Point(i * f, (h - yDown) * f);
+                    double yDown = Y(w, s, calcX) + t / 2.0;
+                    double yUp = h - yDown;
+                    int actualX = i - t / 2;
 
-                    if (x == w) iWhenXIsW = i;
+                    pointsDown[i] = new Point(actualX * f, (int)(yDown * f));
+                    pointsUp[pointsDown.Length - 1 - i] = new Point((w - actualX) * f, (int)(yDown * f));
+                    pointsUp[i] = new Point(actualX * f, (int)(yUp * f));
+                    pointsDown[pointsDown.Length - 1 - i] = new Point((w - actualX) * f, (int)(yUp * f));
+
+                    if (Math.Abs(actualX) < 0.6)
+                    {
+                        yEnd = (int)yDown;
+                    }
                 }
 
-                g.DrawLines(new Pen(drawColor, t * f), pointsDown);
+                g.DrawLines(new Pen(color.Color, t * f), pointsDown);
 
                 g.DrawLines(new Pen(deleteColor, t * 2 * f), pointsUp);
-                g.DrawLines(new Pen(drawColor, t * f), pointsUp);
+                g.DrawLines(new Pen(color.Color, t * f), pointsUp);
 
-                yEnd = Y(w - t, s, iWhenXIsW) + t;
             }
-
+            // /*
             for (int i = 0; i < bmp.Width; i++)
             {
                 for (int j = 0; j < bmp.Height; j++)
                 {
-                    Color color = bmp.GetPixel(i, j);
+                    Color pixel = bmp.GetPixel(i, j);
 
-                    if (color.A == deleteColor.A && color.B == deleteColor.B &&
-                        color.G == deleteColor.G && color.R == deleteColor.R)
+                    if (pixel.A == deleteColor.A && pixel.B == deleteColor.B &&
+                        pixel.G == deleteColor.G && pixel.R == deleteColor.R)
                     {
                         bmp.SetPixel(i, j, Color.Transparent);
                     }
@@ -138,19 +138,19 @@ namespace ShuffleIcon
             return bmp;
         }
 
-        private static int Y(int b, double s, int x)
+        private static double Y(double b, double s, double x)
         {
-            return Convert.ToInt32((b * Math.Pow(s, x)) / (Math.Pow(s, x) + b - 1));
+            return b * Math.Pow(s, x) / (Math.Pow(s, x) + b - 1);
         }
 
-        private static int MiddleX(int b, double s)
+        private static double MiddleX(int b, double s)
         {
             bool isLower = true;
-            int x = 0, y = 0, step = 100;
+            double x = 0, y = 0, step = 100, half = b / 2.0;
 
             do
             {
-                if (y < b / 2)
+                if (y < half)
                 {
                     if (!isLower) step /= 10;
 
@@ -167,7 +167,7 @@ namespace ShuffleIcon
 
                 y = Y(b, s, x);
 
-            } while (y != b / 2 && step > 0);
+            } while (Math.Abs(y - half) > 0.01 && step > 0);
 
             return x;
         }
