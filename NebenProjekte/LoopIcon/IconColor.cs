36968ea35a4a0f5aa111ea3bca7d074c0519eae1
Clemens Ott <clemens.ott@edu.fh-joanneum.at>
2019-05-13 09:17:59 +0200

    Updated LoopIcon generator

---
+++ NebenProjekte/LoopIcon/IconColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace LoopIcon
+{
+    struct IconColor
+    {
+        public string Name { get; }
+
+        public Color Color { get; }
+
+        public Brush Brush { get; }
+
+        public bool WithOne { get; }
+
+        public IconColor(string name, Color color, bool withOne)
+        {
+            Name = name;
+            Color = color;
+            Brush = new SolidBrush(Color);
+            WithOne = withOne;
+        }
+    }
+}
